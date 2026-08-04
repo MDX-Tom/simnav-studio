d452fe70d46dfa9a93c9da7041cb16ca1b488ba0
Mi Tom <6468993+MDX-Tom@users.noreply.github.com>
2026-08-04 12:20:39 +0800

    ui: bug fix package 001/006/013

--- NavPlanner/Resources/Web/pages/plan.js
+++ NavPlanner/Resources/Web/pages/plan.js
@@ -3,11 +3,13 @@ export function registerPlanPage(context) {
     elements,
     buildRoute,
     clearAllMapDrawings,
+    resetAndReplan,
     stopActiveRouteOperation,
   } = context;
 
   elements.planButton?.addEventListener("click", buildRoute);
   elements.recalculateButton?.addEventListener("click", () => buildRoute({ forceAuto: true }));
   elements.planClearTrackButton?.addEventListener("click", clearAllMapDrawings);
+  elements.resetAndReplanButton?.addEventListener("click", resetAndReplan);
   elements.stopRequestButton?.addEventListener("click", stopActiveRouteOperation);
 }
